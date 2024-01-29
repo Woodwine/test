d6341119e99ff6bf096ea0839a542290fb8ac313
Woodwine <sd@myexe.net>
2024-01-29 19:52:51 +0300

    Add comment

--- src/main/java/ru/yandex/market/BasePage.java
+++ src/main/java/ru/yandex/market/BasePage.java
@@ -5,6 +5,8 @@ import io.qameta.allure.Step;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BasePage {
+
+    // предусловие
     @Step("Переходим на сайт {url}")
     public static <T extends BasePage> T openPage(String url, Class<T> typeNextPage) {
         return open(url, typeNextPage);
