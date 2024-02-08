161883dd770162a02046aabb5cac473f6a43889d
Woodwine <sd@myexe.net>
2024-02-08 17:50:28 +0300

    Change configuration

--- src/test/java/ru/yandex/market/BaseTests.java
+++ src/test/java/ru/yandex/market/BaseTests.java
@@ -2,11 +2,25 @@ package ru.yandex.market;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTests {
 
     @BeforeAll
     public static void setup() {
-        Configuration.baseUrl = "http://localhost:8000";
+        Configuration.remote = "http://localhost:4444/wd/hub/";
+        Configuration.timeout = 10000;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", false);
+
+        capabilities.setCapability("selenoid:options", selenoidOptions);
+        Configuration.browserCapabilities = capabilities;
     }
 }
